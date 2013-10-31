0aa51856cb2fb9beb3eeabbf9422a6a9421f976d
Andrew Chambers <Andrew@andrew-desktop.(none)>
2013-10-31 14:33:52 +1300

    added terminal code

--- src/main.c
+++ src/main.c
@@ -2,11 +2,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <termios.h>
 #include <unistd.h>
 
 
 
 pthread_mutex_t emu_mutex;
+
+
+
+
+int ttyraw()
+{
+	int fd = STDIN_FILENO;
+	struct termios oldtermios;
+	/* Set terminal mode as follows:
+	   Noncanonical mode - turn off ICANON.
+	   Turn off signal-generation (ISIG)
+	   	including BREAK character (BRKINT).
+	   Turn off any possible preprocessing of input (IEXTEN).
+	   Turn ECHO mode off.
+	   Disable CR-to-NL mapping on input.
+	   Disable input parity detection (INPCK).
+	   Disable stripping of eighth bit on input (ISTRIP).
+	   Disable flow control (IXON).
+	   Use eight bit characters (CS8).
+	   Disable parity checking (PARENB).
+	   Disable any implementation-dependent output processing (OPOST).
+	   One byte at a time input (MIN=1, TIME=0).
+	*/
+	struct termios newtermios;
+	if(tcgetattr(fd, &oldtermios) < 0)
+		return 1;
+	newtermios = oldtermios;
+
+	newtermios.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	/* OK, why IEXTEN? If IEXTEN is on, the DISCARD character
+	   is recognized and is not passed to the process. This 
+	   character causes output to be suspended until another
+	   DISCARD is received. The DSUSP character for job control,
+	   the LNEXT character that removes any special meaning of
+	   the following character, the REPRINT character, and some
+	   others are also in this category.
+	*/
+
+	newtermios.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	/* If an input character arrives with the wrong parity, then INPCK
+	   is checked. If this flag is set, then IGNPAR is checked
+	   to see if input bytes with parity errors should be ignored.
+	   If it shouldn't be ignored, then PARMRK determines what
+	   character sequence the process will actually see.
+	   
+	   When we turn off IXON, the start and stop characters can be read.
+	*/
+
+	newtermios.c_cflag &= ~(CSIZE | PARENB);
+	/* CSIZE is a mask that determines the number of bits per byte.
+	   PARENB enables parity checking on input and parity generation
+	   on output.
+	*/
+
+	newtermios.c_cflag |= CS8;
+	/* Set 8 bits per character. */
+
+	newtermios.c_oflag &= ~(OPOST);
+	/* This includes things like expanding tabs to spaces. */
+
+	newtermios.c_cc[VMIN] = 1;
+	newtermios.c_cc[VTIME] = 0;
+
+	/* You tell me why TCSAFLUSH. */
+	if(tcsetattr(fd, TCSAFLUSH, &newtermios) < 0)
+		return -1;
+	return 0;
+}
 
 
 void * runEmulator(void * p) {
@@ -42,6 +111,10 @@ int main(int argc,char * argv[]) {
         
     pthread_t emu_thread;
     
+	
+	
+	
+	
     if(pthread_mutex_init(&emu_mutex,NULL)) {
         puts("failed to create mutex");
         return 1;
@@ -64,12 +137,18 @@ int main(int argc,char * argv[]) {
         return 1;
     }
     
+	if(ttyraw()) {
+		puts("failed to configure raw mode");
+		return 1;
+	}
     
     if(pthread_create(&emu_thread,NULL,runEmulator,emu)) {
         puts("creating emulator thread failed!");
         return 1;
     }
     
+
+	
     while(1) {
         int c = getchar();
         if(c == EOF) {
